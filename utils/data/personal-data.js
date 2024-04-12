dfa375a24705004f6b9f3b30b16e496ef0477b72
PawarAyush <pawarayush2003@gmail>
2024-04-12 17:41:12 +0530

    Second

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "AYUSH PAWAR",
   profile: '/profile1.jpg',
   designation: "Software Developer",
-  description: "My name is AYUSH PAWAR. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "My name is AYUSH PAWAR. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of application development and love to make the Applications more open to the world. My core skill is based on Kotlin/Java and I love to do most of the things using Kotlin/Java. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'pawarayush2003@gmail.com',
   phone: '+9191004169',
   address: 'Jabalpur mp INDIA',
